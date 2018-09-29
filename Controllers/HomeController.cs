7ee7ecda7fddc514f5bc346f9e1bf36d4a79bddb
Jeferson da Silva Araujo <jeff.araujo@gmail.com>
2018-09-29 11:07:03 -0300

    Adicionando Views na aplicação!

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,9 +8,9 @@ namespace Jeffao_Web.Controllers
 {
     public class HomeController: Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return DateTime.Now.ToString();
+            return View();
         }
 
     }
